6b74a43a8c029ca492e4f62fc0293535837c3c8f
prickol-maniac <ya.alinka2003@yandex.ru>
2022-10-07 22:11:49 +0300

    Отправим прак куда-нибудь в инет

---
+++ Wpf_pr1/Commands/DataCommands.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Wpf_pr1.Commands
+{
+    public class DataCommands
+    {
+        public static RoutedCommand Delete { get; set; }
+        public static RoutedCommand Edit {get; set;}
+        static DataCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.E, ModifierKeys.Control, "Ctrl+E"));
+            Edit = new RoutedCommand("Edit", typeof(DataCommands), inputs);
+            inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.D, ModifierKeys.Control, "Ctrl+0"));
+            Delete = new RoutedCommand("Delete", typeof(DataCommands), inputs);
+        }
+    }
+}
---
+++ Wpf_pr1/PageEmployee.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_pr1
+{
+    /// <summary>
+    /// Логика взаимодействия для PageEmployee.xaml
+    /// </summary>
+    public partial class PageEmployee : Page
+    {
+        public PageEmployee()
+        {
+            InitializeComponent();
+        }
+
+        private bool isDirty = true;
+
+        private void UndoCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !isDirty;
+        }
+
+        private void UndoCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Отмена");
+            isDirty = true;
+        }
+
+        private void NewCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+
+        private void NewCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Создать");
+            isDirty = true;
+        }
+
+        private void SaveCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = !isDirty;
+        }
+
+        private void SaveCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Сохранить");
+            isDirty = true;
+        }
+
+        private void FindCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+
+        private void FindCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Найти");
+            isDirty = true;
+        }
+
+        private void EditCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+
+        private void EditCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Изменить");
+            isDirty = true;
+        }
+
+        private void DeleteCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = isDirty;
+        }
+
+        private void DeleteCommandBinding_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("Удалить");
+            isDirty = true;
+        }
+    }
+}
